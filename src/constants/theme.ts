8b811f81675b6a3db9b918406133140ae10a0bf6
Joshua Galeria <87725131+jgaleria@users.noreply.github.com>
2025-05-31 22:34:25 -0700

    Clean folder structure

---
+++ src/constants/theme.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  background: '#FFFFFF',
+  primary: '#000000',
+  text: {
+    secondary: '#666666',
+  },
+};
+
+export const TYPOGRAPHY = {
+  sizes: {
+    lg: 18,
+    '3xl': 32,
+  },
+  weights: {
+    bold: '700',
+  },
+};
+
+export const SPACING = {
+  sm: 8,
+}; 
